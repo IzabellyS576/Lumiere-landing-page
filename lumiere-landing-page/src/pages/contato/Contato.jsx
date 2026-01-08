036d4e75f0678f65aaed53a71258257daf0cae03
Belly <izabellysouza576@gmail.com>
2026-01-08 20:50:18 -0300

    feat: concluindo form funcional

--- lumiere-landing-page/src/pages/contato/Contato.jsx
+++ lumiere-landing-page/src/pages/contato/Contato.jsx
@@ -1,37 +1,72 @@
 import { motion } from 'motion/react'
-import { useState } from "react";
 import TextareaAutosize from "react-textarea-autosize"
+import Swal from 'sweetalert2'
 
 import Header from "../../components/header/Header";
 import styles from "./Contato.module.css"
 
 function Contato(){
-    const [name,setName] = useState();
-    const [email, setEmail] = useState();
-    const [msg, setMsg] = useState();
+    
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    formData.append("access_key", "7f405ec1-6bf4-40ac-9ea6-5ea572bcf0b5");
+
+   try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        Swal.fire({
+            title: "Enviado!",
+            text: "Obrigado pela mensagem!",
+            icon: "success",
+        });
+        event.target.reset(); 
+      } else {
+        console.log("Erro", data);
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Algo deu errado.",
+        });
+      }
+    } catch (error) {
+      console.log("Erro de requisição", error);
+      Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Ocorreu um erro ao enviar.",
+        });
+    }
+  };
+
     return(
         <>
             <Header title='Contato' subtitle='Conte conosco para tornar sua experiência ainda melhor' />
             <section className={styles.section}>
-                <form className={styles.form}>
+                <form className={styles.form} onSubmit={onSubmit} >
                     <h3>Conecte-se</h3>
                     <h4>Estamos à disposição para esclarecer suas dúvidas.</h4>
                     <div className={styles.grupo_input}>
-                        <input type="text" value={name} id="nome" placeholder=" " required onSubmit={(e)=>{setName(e.target.value); e.preventDefault()}} ></input>
+                        <input type="text" name='nome' id="nome" placeholder=" " required ></input>
                         <label htmlFor="nome">Nome</label>
                     </div>
                     <div className={styles.grupo_input}>
-                        <input type='email' value={email} id="email" placeholder=" " required onSubmit={(e)=>{setEmail(e.target.value); e.preventDefault()}} ></input>
+                        <input type='email' name='email' id="email" placeholder=" " required ></input>
                         <label htmlFor="email">Email</label>
                     </div>
                     <div className={styles.grupo_input}>
                         <TextareaAutosize
                             id='mensagem'
+                            name='mensagem'
                             placeholder=" "
                             required
-                            value={msg}
                             minRows={4}
-                            onChange={(e)=>(setMsg(e.target.value))}
                             style={{overflow: "hidden", padding: '1rem'}}
                         />
                         <label htmlFor="mensagem">Mensagem</label>
