88c33b5dc43d585e69e1bc47a78211e986d66156
Belly <izabellysouza576@gmail.com>
2026-01-11 21:33:45 -0300

    style: concluindo responsividade de Produtos

--- lumiere-landing-page/src/components/produtosComponents/Carousel.jsx
+++ lumiere-landing-page/src/components/produtosComponents/Carousel.jsx
@@ -16,13 +16,38 @@ function Carousel({ prodType }) {
     <Swiper
       // install Swiper modules
       modules={[Navigation, A11y]}
-      spaceBetween={5}
-      slidesPerView={4.2}
+      spaceBetween={20}
+      slidesPerView={1.8}
       draggable
       navigation
       centeredSlides
       loop={true}
-      
+      breakpoints={{
+          538: {
+            slidesPerView: 1.4, 
+            spaceBetween: 20,
+          },
+          600:{
+            slidesPerView: 1.8,
+            spaceBetween: 20
+          },
+          722: {
+            slidesPerView: 3.2,
+            spaceBetween: 20,
+          },
+          924: {
+            slidesPerView: 3.5, 
+            spaceBetween: 20,
+          },
+          1052: {
+            slidesPerView: 3.8, 
+            spaceBetween: 24,
+          },
+          1280: {
+            slidesPerView: 4.4, 
+            spaceBetween: 28,
+          },
+        }}
     >
       {(Object.values(prodType).map((product)=>(
         <SwiperSlide>
